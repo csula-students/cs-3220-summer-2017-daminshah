7d04b8f6808543fb5f34ff81ce6770ce54179546
daminshah <daminshah210@gmail.com>
2017-08-11 23:36:19 -0700

    Added schema

---
+++ cs3220-homework4/schema7.sql
@@ -0,0 +1,48 @@
+
+
+
+CREATE TABLE menu (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    description TEXT,
+    imgURL TEXT,
+    price DOUBLE
+);
+
+
+CREATE TABLE orders (
+	order_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	customer_name VARCHAR(255) NOT NULL,
+	status VARCHAR(255) NOT NULL
+);
+
+
+INSERT INTO menu (id, name, description, imgURL, price) VALUES
+(1, "Pizza", "Yummy", "images/pizza1.png", 6.99),
+(2, "Hamburger", "Delicious", "images/hamburger1.png", 6.99),
+(3, "Cheese Burger", "Mouth-Watering", "images/burger1.png", 5.99);
+
+
+INSERT INTO orders (order_id, customer_name, status) VALUES
+(1, "Eric", "IN PROGRESS"),
+(2, "Damin", "IN QUEUE");
+
+
+CREATE TABLE orders (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    item VARCHAR(255) NOT NULL,
+    price DOUBLE NOT NULL,
+    customer VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE carts (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    item VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    price DOUBLE NOT NULL
+);
+
+
